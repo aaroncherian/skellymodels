1502ca846f0b22f11ffda8f7c798ac1a6600d8ea
aaroncherian <aaron.cherian2@gmail.com>
2024-05-16 16:34:24 -0400

    Update create_model_skeleton.py

--- skellymodels/create_model_skeleton.py
+++ skellymodels/create_model_skeleton.py
@@ -4,24 +4,19 @@ from skellymodels.model_info.model_info import ModelInfo
 
 def create_mediapipe_skeleton_model():
 
-    from skellymodels.model_info.mediapipe_model_info import MediapipeModelInfo 
+        from skellymodels.model_info.mediapipe_model_info import MediapipeModelInfo 
 
-    """
-    Creates a skeleton model using the mediapipe model
-    Returns:
-    - An instance of the Skeleton class that represents the complete skeletal model 
-    """
+        """
+        Creates a skeleton model using the mediapipe model
+        Returns:
+        - An instance of the Skeleton class that represents the complete skeletal model 
+        """
 
-    mediapipe_model_info = MediapipeModelInfo()
-    skeleton_model = create_skeleton_model(actual_markers=mediapipe_model_info.landmark_names, 
-                      num_tracked_points=mediapipe_model_info.num_tracked_points,
-                      segment_connections=mediapipe_model_info.segment_connections,
-                     virtual_markers=mediapipe_model_info.virtual_markers_definitions,
-                     joint_hierarchy= mediapipe_model_info.joint_hierarchy,
-                     center_of_mass_info=mediapipe_model_info.center_of_mass_definitions
-                )
-    
-    return skeleton_model
+        skeleton_model = create_skeleton_from_this_model_info(
+            model_info=MediapipeModelInfo()
+        )
+
+        return skeleton_model
 
 
 def create_qualisys_skeleton_model():
@@ -33,15 +28,10 @@ def create_qualisys_skeleton_model():
         Returns:
         - An instance of the Skeleton class that represents the complete skeletal model 
         """
-    
-        qualisys_model_info = QualisysModelInfo()
-        skeleton_model = create_skeleton_model(actual_markers=qualisys_model_info.landmark_names, 
-                        num_tracked_points=qualisys_model_info.num_tracked_points,
-                        segment_connections=qualisys_model_info.segment_connections,
-                        virtual_markers=qualisys_model_info.virtual_markers_definitions,
-                        joint_hierarchy= qualisys_model_info.joint_hierarchy,
-                        center_of_mass_info=qualisys_model_info.center_of_mass_definitions
-                    )   
+
+        skeleton_model = create_skeleton_from_this_model_info(
+            model_info=QualisysModelInfo()
+        )
         
         return skeleton_model
 
@@ -50,7 +40,7 @@ def create_qualisys_mdn_nih_skeleton_model():
       
       from skellymodels.model_info.qualisys_model_info import QualisysMDN_NIHModelInfo
 
-      skeleton_model = create_this_model_skeleton(
+      skeleton_model = create_skeleton_from_this_model_info(
             model_info=QualisysMDN_NIHModelInfo()
       )
 
@@ -58,7 +48,7 @@ def create_qualisys_mdn_nih_skeleton_model():
 
 
 
-def create_this_model_skeleton(model_info:ModelInfo):
+def create_skeleton_from_this_model_info(model_info:ModelInfo):
       
         """
         Creates a skeleton model using the qualisys model
