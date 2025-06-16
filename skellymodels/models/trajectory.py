050a2dffd56da49b9b123a61194beed295640cdc
Aaron Cherian <aaron.cherian2@gmail.com>
2025-06-16 18:14:52 -0400

    create_trajectory_builder

--- skellymodels/models/trajectory.py
+++ skellymodels/models/trajectory.py
@@ -14,8 +14,6 @@ class TrajectoryValidator(BaseModel):
         if self.data.shape[1] != len(self.marker_names):
             raise ValueError(f"Trajectory data must have the same number of markers as input name list. Data has shape {self.data.shape} and list has {len(self.marker_names)} markers.")
 
-    
-
 class Trajectory:
     """
     A container for 3D motion capture trajectory data that handles both tracked markers 
@@ -44,25 +42,29 @@ class Trajectory:
                  marker_names: List[MarkerName], 
                  virtual_marker_definitions: Dict[str, VirtualMarkerDefinition] | None = None, 
                  segment_connections: Dict[SegmentName, SegmentConnection] | None = None):
+        
         self.name = name
         self._trajectories = {}
         self._tracked_point_names = marker_names
         self._virtual_marker_definitions = virtual_marker_definitions
         self._segment_connections = segment_connections
+
         self._validate_data(data=data, marker_names=self._tracked_point_names)
         self._set_trajectory_data(data=data, marker_names=self._tracked_point_names, virtual_marker_definitions=virtual_marker_definitions)
+        
         self._num_frames = data.shape[0]
 
-    def _validate_data(self, data: np.ndarray, marker_names: List[str]):
+    def _validate_data(self, data: np.ndarray, marker_names: List[MarkerName]):
         TrajectoryValidator(data=data, marker_names= marker_names)
 
-    def _set_trajectory_data(self, data:np.ndarray, marker_names:List[str], virtual_marker_definitions: Dict = None):
+    def _set_trajectory_data(self, data:np.ndarray, marker_names:List[MarkerName], virtual_marker_definitions: Dict[str, VirtualMarkerDefinition] = None):
         self._trajectories.update({marker_name: data[:, i, :] for i, marker_name in enumerate(marker_names)})
         self._marker_names = marker_names.copy()
         
         if virtual_marker_definitions:
             print(f'Calculating virtual markers: {list(virtual_marker_definitions.keys())}')
             virtual_marker_data = {}
+
             for vm_name, vm_info in virtual_marker_definitions.items():
                 vm_positions = np.zeros((data.shape[0], 3))
                 for marker_name, weight in zip(vm_info["marker_names"], vm_info["marker_weights"]):
@@ -157,3 +159,47 @@ class Trajectory:
     def __repr__(self) -> str:
         return self.__str__()
 
+
+class TrajectoryBuilder:
+
+    def __init__(self,
+                name: str, 
+                tracked_point_names: List[MarkerName],
+                virtual_marker_definitions: Dict[str, VirtualMarkerDefinition]|None = None,
+                segment_connections: Dict|None = None):
+        self.name = name
+        self.tracked_point_names = tracked_point_names
+        self.virtual_marker_definitions = virtual_marker_definitions
+        self.segment_connections = segment_connections
+
+    def build(
+            self,
+            data_array: np.ndarray
+    ):
+        landmark_names = self.tracked_point_names.copy()
+        output_array_as_list: list[np.ndarray] = [data_array]
+
+        if self.virtual_marker_definitions:
+            for vm_name, vm_components in self.virtual_marker_definitions.items():
+                component_names = vm_components["marker_names"]
+                component_weights = vm_components["marker_weights"]
+
+                component_indices = [self.tracked_point_names.index(name) for name in component_names]
+                component_data = data_array[:,component_indices,:]
+                component_weights = component_weights[None, :, None]
+                weighted_marker_data = component_data * component_weights
+                virtual_marker = np.sum(weighted_marker_data, axis = 1)
+                output_array_as_list.append(virtual_marker[:, None, :])
+                landmark_names.append(vm_name) 
+
+        output_array = np.concatenate(output_array_as_list, axis=1) if len(output_array_as_list) > 1 else data_array
+
+        return Trajectory(
+            array = output_array,
+            name = self.name,
+            landmark_names = landmark_names,
+            tracked_point_names = self.tracked_point_names,
+            virtual_marker_definitions= self.virtual_marker_definitions,
+            segment_connections = self.segment_connections,
+
+    )
