97f782e6c58e47e16d4a28b79d74cba65f60baae
Aaron Cherian <aaron.cherian2@gmail.com>
2025-06-26 16:30:46 -0400

    fix board

--- skellymodels/managers/board.py
+++ skellymodels/managers/board.py
@@ -2,14 +2,13 @@ from enum import Enum
 from skellymodels.managers.actor import Actor
 from skellymodels.models.tracking_model_info import ModelInfo
 from skellymodels.models.aspect import Aspect
-import numpy as np
-
+from pathlib import Path
 ##NOTE: Playing around with the idea of making a Board Actor. Untested, but I think this could be extended to 
 ## keep track of 7x5 and 5x3 data (or take in those parameters) when saving out to automatically create the correct Board
 ## will return to this later
 
 class BoardAspectEnum(Enum):
-    BOARD = "board"
+    BODY = "body"
 
 class Board(Actor):
     def __init__(self, name:str, model_info:ModelInfo):
@@ -18,21 +17,28 @@ class Board(Actor):
     
     def _add_board(self):
         self.aspect_from_model_info(
-            name = BoardAspectEnum.BOARD.value
+            name = BoardAspectEnum.BODY.value
         )
 
     @property
-    def board(self) -> Aspect:
-        return self.aspects[BoardAspectEnum.BOARD.value]
+    def body(self) -> Aspect:
+        return self.aspects[BoardAspectEnum.BODY.value]
     
     def add_tracked_points_numpy(self, tracked_points_numpy_array):
-        self.board.add_tracked_points(
-            tracked_points_numpy_array[:, self.tracked_point_slices[BoardAspectEnum.BOARD.value],:]
+        self.body.add_tracked_points(
+            tracked_points_numpy_array[:, self.tracked_point_slices[BoardAspectEnum.BODY.value],:]
         )
 
     @classmethod
-    def from_board_dimensions(rows:int, columns:int):
-        """
-        potential example of how we could build a board from the input dimensions (will return to this later)
-        """
-        pass
+    def from_board_definition(cls, columns: int, rows:int):
+        board_yaml = f"charuco_board_{columns}_{rows}.yaml"
+        try:
+            path_to_yaml = Path(__file__).parents[1]/'tracker_info'/board_yaml
+            charuco_model_info = ModelInfo.from_config_path(path_to_yaml)
+            board = cls(
+                name = f"charuco_board_{columns}_{rows}",
+                model_info = charuco_model_info
+            )
+            return board
+        except FileNotFoundError:
+            print("Charuco board definition not recognized")
